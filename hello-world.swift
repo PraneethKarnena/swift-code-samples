7f841689e5f6e872a025abd0c4a5173a02c0b255
Praneeth Karnena <hello@kspraneeth.com>
2017-12-16 09:06:35 +0530

    Create hello-world.swift

---
+++ hello-world.swift
@@ -0,0 +1,14 @@
+/*
+
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, 
+and finally print the value of your variable on a second line.
+
+*/
+
+let inputString = readLine()! // get a line of input from stdin and save it to our variable
+
+// Your first line of output goes here
+print("Hello, World.")
+
+// Write the second line of output
+print(inputString)
